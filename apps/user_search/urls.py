79935af1463c0e4835412178fbe32a172a228af9
s-whale <fullback@nwafu.edu.cn>
2025-07-02 23:44:14 +0800

    新增数据处理统计接口，供前端首页渲染

--- apps/user_search/urls.py
+++ apps/user_search/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import searchForDataList, searchForDataDetail
+from .views import searchForDataList, searchForDataDetail, dataProcessStatistics
 
 urlpatterns = [
     path('data-list/', searchForDataList.as_view(), name='searchForDataList'),
-    path('data-detail/', searchForDataDetail.as_view(), name='searchForDataDetail')
+    path('data-detail/', searchForDataDetail.as_view(), name='searchForDataDetail'),
+    path('data-process-statistics/', dataProcessStatistics.as_view(), name='dataProcessStatistics')
 ]
